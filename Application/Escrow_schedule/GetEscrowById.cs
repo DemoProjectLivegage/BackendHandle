4ebb407884292cbddba1959c71f5c60f10d5e7e8
AKewat@livegage <-AKewat@livegage>
2023-08-25 12:54:19 +0530

    bool resolve

--- Application/Escrow_schedule/GetEscrowById.cs
+++ Application/Escrow_schedule/GetEscrowById.cs
@@ -26,10 +26,14 @@ namespace Application.Escrow_schedule
 
             public async Task<List<Escrow_Disbursement_Schedule>> Handle(Query request, CancellationToken cancellationToken)
             {
+                Payment_Schedule newList = await this.context.Payment_Schedule.FindAsync(request.Id);
+                if(!newList.Escrow) return null;
+                
                 List<Escrow_Disbursement_Schedule> list = await this.context.Escrow_Disbursement_Schedule.ToListAsync();
-                list.OrderBy(a => a.Loan_Id).GroupBy(x => x.Loan_Id);
-                list.OrderBy(x => x.date);
-                var byId = list.Find(x => x.Loan_Id == request.Id);
+                // list.OrderBy(a => a.Loan_Id).GroupBy(x => x.Loan_Id);
+                // list.OrderBy(x => x.date);
+                // var byId = list.Find(x => x.Loan_Id == request.Id);
+                list = list.FindAll(a=>a.Loan_Id==request.Id).ToList();
                 return list;
             }
         }
--- Application/Escrow_schedule/UpdateEscrowAmount.cs
+++ Application/Escrow_schedule/UpdateEscrowAmount.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MediatR;
+using Persistence;
 
 namespace Application.Escrow_schedule
 {
@@ -10,7 +11,21 @@ namespace Application.Escrow_schedule
     {
         public class Command : IRequest {
             public int loanId {get; set;}
-            List<Escrow_Benificiary> benificiaries {get; set;}
+            public List<Escrow_Benificiary> benificiaries {get; set;}
+        }
+        public class Handle : IRequestHandler<Command>
+        {
+            private readonly DatabaseContext context;
+
+            public Handle(DatabaseContext context)
+            {
+                this.context = context;
+            }
+
+            Task<Unit> IRequestHandler<Command, Unit>.Handle(Command request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
