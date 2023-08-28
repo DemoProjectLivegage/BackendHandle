f7831a0233d3e50f05b04a167cb1518463198e98
AKewat@livegage <-AKewat@livegage>
2023-08-28 14:49:38 +0530

    some modifiation on EscrowDisbursement

--- API/Controllers/EscrowDisbursementController.cs
+++ API/Controllers/EscrowDisbursementController.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Controllers.DTO;
 using Application.DTO;
 using Application.Escrow_schedule;
 using Domain;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
